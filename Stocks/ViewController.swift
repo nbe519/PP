d142858f9a1156185f253140ddfd76709602c780
Noah <noaheaton@noahs-macbook-pro.local>
2018-08-21 14:20:42 -0400

    Initial Commit

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Noah Eaton on 8/21/18.
+//  Copyright © 2018 Noah Eaton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
